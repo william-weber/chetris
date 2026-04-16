c5c47f393f4541a7e95b94dc5f36b7fa3fed0715
william-weber <taj.tek@gmail.com>
2026-04-16 16:12:22 -0400

    calendar animates, renders real dates

--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -1,7 +1,26 @@
 import { ChevronLeft } from "lucide-react";
 import { ChevronRight } from "lucide-react";
+import { useState } from "react";
+import { startOfMonth, subMonths, addMonths, getDaysInMonth } from "date-fns";
 
 const CalendarPage = () => {
+  const [now, setNow] = useState(() => {
+    return new Date();
+  });
+
+  const decrementMonth = () => {
+    setNow((prev) => subMonths(prev, 1));
+  };
+  const incrementMonth = () => {
+    setNow((prev) => addMonths(prev, 1));
+  };
+
+  const getBlankDays = () => {
+    const monthStart = startOfMonth(now);
+    const blankDays = monthStart.getDay(); // 0 (Sun) to 6 (Sat)
+    return blankDays;
+  };
+
   return (
     <div className="antialiased sans-serif h-screen text-base-100">
       <div className="container mx-auto px-4 py-2">
@@ -10,18 +29,24 @@ const CalendarPage = () => {
           <div className="flex items-center justify-between py-2 px-6">
             <div>
               <span className="text-lg font-bold text-base-content/80">
-                April
+                {now.toLocaleString("default", { month: "long" })}
               </span>
               <span className="ml-1 text-lg text-base-content/70 font-normal">
-                2026
+                {now.getFullYear()}
               </span>
             </div>
             <div className="border rounded-lg px-1">
-              <button className="leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center">
+              <button
+                className="leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center"
+                onClick={decrementMonth}
+              >
                 <ChevronLeft className="h-6 w-6 text-base-content/70" />
               </button>
-              <div class="border-r inline-flex h-6"></div>
-              <button className="leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center">
+              <div className="border-r inline-flex h-6"></div>
+              <button
+                className="leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-200 p-1 items-center"
+                onClick={incrementMonth}
+              >
                 <ChevronRight className="h-6 w-6 text-base-content/70" />
               </button>
             </div>
@@ -41,33 +66,34 @@ const CalendarPage = () => {
             </div>
             {/* calendar days */}
             <div className="flex flex-wrap border-t border-l text-base-content/30 border-base-content/10">
-              {/* 2 blank days e.g. hardcoded for now */}
-              <div
-                style={{ width: "14.28%", height: "120px" }}
-                className="text-center border-r border-b border-base-content/10 px-4 pt-2"
-              ></div>
-              <div
-                style={{ width: "14.28%", height: "120px" }}
-                className="text-center border-r border-b border-base-content/10 px-4 pt-2"
-              ></div>
-              {/* days 1-30 (numbered days) */}
-              {Array.from({ length: 30 }, (_, i) => i + 1).map((day) => (
+              {/* blank days */}
+              {Array.from({ length: getBlankDays() }).map((_, i) => (
                 <div
-                  key={day}
+                  key={i}
                   style={{ width: "14.28%", height: "120px" }}
-                  className="border-r border-b border-base-content/10 px-4 pt-2 relative"
-                >
-                  <div className="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-base-content/70 hover:bg-base-content/10 absolute top-1 right-1">
-                    {day}
-                  </div>
-                  <div
-                    style={{ height: "80px" }}
-                    className="overflow-y-auto mt-5 text-xs text-left text-base-content/70"
-                  >
-                    test event
-                  </div>
-                </div>
+                  className="text-center border-r border-b border-base-content/10 px-4 pt-2"
+                ></div>
               ))}
+              {/* numbered days */}
+              {Array.from({ length: getDaysInMonth(now) }, (_, i) => i + 1).map(
+                (day) => (
+                  <div
+                    key={day}
+                    style={{ width: "14.28%", height: "120px" }}
+                    className="border-r border-b border-base-content/10 px-4 pt-2 relative"
+                  >
+                    <div className="inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 text-base-content/70 hover:bg-base-content/10 absolute top-1 right-1">
+                      {day}
+                    </div>
+                    <div
+                      style={{ height: "80px" }}
+                      className="overflow-y-auto mt-5 text-xs text-left text-base-content/70"
+                    >
+                      {/* example events */}
+                    </div>
+                  </div>
+                ),
+              )}
             </div>
           </div>
         </div>
